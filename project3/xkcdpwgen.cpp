17a102f26118fb178ac8ed97bd1ca95fb12e8ca7
Silas Nevstad <115965493+silasnevstad@users.noreply.github.com>
2023-02-28 22:40:00 -0500

    Add files via upload

--- project3/xkcdpwgen.cpp
+++ project3/xkcdpwgen.cpp
@@ -10,8 +10,8 @@
 
 // load words from a file into a vector
 std::vector<std::string> load_words(const std::string& filename) {
-    std::vector<std::string> valid_words;
-    std::ifstream word_file(filename);
+    std::vector<std::string> valid_words; // vector to store the words
+    std::ifstream word_file(filename); // open the file
     if (word_file.is_open()) {
         std::string word;
         while (std::getline(word_file, word)) {
@@ -27,7 +27,7 @@ std::vector<std::string> load_words(const std::string& filename) {
 
 // generate a password using the XKCD method
 std::string generate_password(const std::vector<std::string>& word_list, int num_words, int num_caps, int num_numbers, int num_symbols) {
-    std::vector<std::string> words(num_words);
+    std::vector<std::string> words(num_words); // vector to store the words
     std::default_random_engine rng(time(nullptr)); // initialize the random number generator
     std::uniform_int_distribution<int> dist(0, word_list.size() - 1); // create a distribution to generate random indices
 
@@ -44,55 +44,55 @@ std::string generate_password(const std::vector<std::string>& word_list, int num
 
     // capitalize some of the words
     if (num_caps > 0) {
-        std::uniform_int_distribution<int> cap_dist(0, num_words - 1);
-        std::vector<int> cap_indices(num_caps);
-        std::generate(cap_indices.begin(), cap_indices.end(), [&cap_dist, &rng]() {
+        std::uniform_int_distribution<int> cap_dist(0, num_words - 1); // create a distribution to generate random indices
+        std::vector<int> cap_indices(num_caps); // vector to store the indices of the words to capitalize
+        std::generate(cap_indices.begin(), cap_indices.end(), [&cap_dist, &rng]() { // generate the indices
             return cap_dist(rng);
         });
-        std::sort(cap_indices.begin(), cap_indices.end());
-        for (const auto& index : cap_indices) {
+        std::sort(cap_indices.begin(), cap_indices.end()); // sort the indices
+        for (const auto& index : cap_indices) { // capitalize the words
             words[index][0] = toupper(words[index][0]);
         }
         password = "";
-        for (const auto& word : words) {
+        for (const auto& word : words) { // join the words together with spaces again so it can be split later
             password += word + " ";
         }
     }
 
     // insert some numbers into the password
     if (num_numbers > 0) {
-        std::uniform_int_distribution<int> num_dist(0, num_words);
-        std::vector<int> insert_positions(num_numbers);
-        std::generate(insert_positions.begin(), insert_positions.end(), [&num_dist, &rng]() {
-            return num_dist(rng);
+        std::uniform_int_distribution<int> num_dist(0, num_words); // create a distribution to generate random indices
+        std::vector<int> insert_positions(num_numbers); // vector to store the indices of the words to insert numbers into
+        std::generate(insert_positions.begin(), insert_positions.end(), [&num_dist, &rng]() { // generate the indices
+            return num_dist(rng); 
         });
-        std::sort(insert_positions.begin(), insert_positions.end());
+        std::sort(insert_positions.begin(), insert_positions.end()); // sort the indices
         int num_inserted = 0;
-        for (const auto& pos : insert_positions) {
+        for (const auto& pos : insert_positions) { // insert the numbers
             int insert_pos = 0;
-            for (int i = 0; i < pos; ++i) {
-                insert_pos += words[i].size() + 1;
+            for (int i = 0; i < pos; ++i) { // calculate the position to insert the number
+                insert_pos += words[i].size() + 1; // add the length of the word and a space
             }
-            password.insert(insert_pos + num_inserted, std::to_string(rng() % 10));
+            password.insert(insert_pos + num_inserted, std::to_string(rng() % 10)); // insert the number
             ++num_inserted;
         }
     }
 
     // insert some symbols into the password
     if (num_symbols > 0) {
-        std::uniform_int_distribution<int> symbol_dist(0, num_words);
+        std::uniform_int_distribution<int> symbol_dist(0, num_words); // create a distribution to generate random indices
         std::vector<int> insert_positions(num_symbols);
-        std::generate(insert_positions.begin(), insert_positions.end(), [&symbol_dist, &rng]() {
+        std::generate(insert_positions.begin(), insert_positions.end(), [&symbol_dist, &rng]() { // generate the indices
             return symbol_dist(rng);
         });
-        std::sort(insert_positions.begin(), insert_positions.end());
+        std::sort(insert_positions.begin(), insert_positions.end()); // sort the indices
         int num_inserted = 0;
-        for (const auto& pos : insert_positions) {
+        for (const auto& pos : insert_positions) { // insert the symbols
             int insert_pos = 0;
             for (int i = 0; i < pos; ++i) {
-                insert_pos += words[i].size() + 1;
+                insert_pos += words[i].size() + 1; // calculate the position to insert the symbol
             }
-            password.insert(insert_pos + num_inserted, 1, "~!@#$%^&*.:;"[symbol_dist(rng)]);
+            password.insert(insert_pos + num_inserted, 1, "~!@#$%^&*.:;"[symbol_dist(rng)]); // insert the symbol
             ++num_inserted;
         }
     }
@@ -103,23 +103,6 @@ std::string generate_password(const std::vector<std::string>& word_list, int num
 
 int main(int argc, char* argv[]) {
     int num_words = 4, num_caps = 0, num_numbers = 0, num_symbols = 0;
-    // create a command line argument parser
-    // if (argc > 1) {
-    //     std::istringstream iss(argv[1]);
-    //     iss >> num_words;
-    //     if (argc > 2) {
-    //         std::istringstream iss(argv[2]);
-    //         iss >> num_caps;
-    //         if (argc > 3) {
-    //             std::istringstream iss(argv[3]);
-    //             iss >> num_numbers;
-    //             if (argc > 4) {
-    //                 std::istringstream iss(argv[4]);
-    //                 iss >> num_symbols;
-    //             }
-    //         }
-    //     }
-    // }
     // parse command line arguments, can be in any order and are optional
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
